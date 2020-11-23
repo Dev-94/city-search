d9351a1b8eed22d2a79d59bace8c1b8dec5046a5
Dev Govindji <friendsofdev94@gmail.com>
2020-11-23 17:56:59 -0600

    stubs basic functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
+// Issue: results only show last orbject in array of objects recieved as response
+// Issue: submit has to be clicked twice to show results, first click is to fetch data
+
 function App() {
+
+  let [query, querySet] = useState('')
+  let [results, resultsSet] = useState([])
+  let [response, responseSet] = useState([])
+
+
+  const fetchData = (event) => {
+    event.preventDefault();
+    const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
+    fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        responseSet(json)
+      });
+    response.filter((data) => {
+      if (query == null) {
+        console.log(query, data)
+        resultsSet(data)
+      }
+      else if (data.city.toLowerCase().includes(query.toLowerCase())) {
+        console.log(query, data)
+        resultsSet(data)
+      }
+
+    })
+  }
+
+  const handleInputChange = (e) => {
+    querySet(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={fetchData}>
+        <label>
+          query:
+        <input
+            type="text"
+            value={query}
+            onChange={handleInputChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+
+      <h4>search results for "{query}"</h4>
+      <hr />
+
+      search results:
+      <br />
+
+      <li> {results.city} </li>
+      <li> {results.growth_from_2000_to_2013}</li>
+      <li> {results.latitude}</li>
+      <li> {results.longitude}</li>
+      <li> {results.population}</li>
+      <li> {results.rank}</li>
+      <li> {results.state}</li>
+
     </div>
   );
 }
